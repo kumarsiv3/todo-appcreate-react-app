e2524cf6d4efe0f24df6f42f0168fde8f6b2bb1e
kumarsiv3 <kumar.web3496@gmail.com>
2019-06-07 09:45:58 -0400

    react16.8 with webpack

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Siva Kumar Garapati</h1>
+        <h4>To-Do App</h4>
+        
       </header>
     </div>
   );
